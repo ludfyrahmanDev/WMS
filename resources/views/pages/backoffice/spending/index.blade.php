10615d811bf346d224b1ad1b03f69abb4700ac51
Mochamad Ludfi Rahman <ludfyrahman@Mochamads-MacBook-Pro.local>
2024-01-10 02:50:36 +0700

    saldo

--- resources/views/pages/backoffice/spending/index.blade.php
+++ resources/views/pages/backoffice/spending/index.blade.php
@@ -65,11 +65,106 @@
 
         <!-- BEGIN: Data List -->
         <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
-            <h1 class="text-xl">
-                <label for="" class="text-primary">Total Saldo | <span class="font-bold">{{toThousand($saldo ?? 0)}}</span> | </label>
-                <label for="" class="text-green-600">Total Uang Masuk | <span class="font-bold">{{toThousand($income ?? 0)}}</span> | </label>
-                <label for="" class="text-danger">Total Uang Keluar | <span class="font-bold">{{toThousand($outcome ?? 0)}}</span> | </label>
-            </h1>
+            <div class="overflow-x-auto">
+                <x-base.table>
+                    
+                    <x-base.table.tbody>
+                        <x-base.table.tr>
+                            <x-base.table.td class="border-b dark:border-darkmode-400">
+                                <div class="whitespace-nowrap font-medium">
+                                    Penjualan Lunas
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data penjualan telah dibayar
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 border-b text-right dark:border-darkmode-400 text-green-800">
+                                {{toThousand($sellingCompleted ?? 0)}}
+                            </x-base.table.td>
+                           
+                        </x-base.table.tr>
+                        <x-base.table.tr>
+                            <x-base.table.td class="border-b dark:border-darkmode-400">
+                                <div class="whitespace-nowrap font-medium">
+                                    Penjualan Belum Lunas(Piutang)
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data penjualan belum dibayar
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 border-b text-right dark:border-darkmode-400 text-danger">
+                                {{toThousand($sellingInCompleted ?? 0)}}
+                            </x-base.table.td>
+                            
+                        </x-base.table.tr>
+                        <x-base.table.tr>
+                            <x-base.table.td class="border-b dark:border-darkmode-400">
+                                <div class="whitespace-nowrap font-medium">
+                                    Pembelian Lunas
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data pembelian telah dibayar
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 border-b text-right dark:border-darkmode-400 text-green-800">
+                                {{toThousand($purchaseCompleted ?? 0)}}
+                            </x-base.table.td>
+                            
+                        </x-base.table.tr>
+                        <x-base.table.tr>
+                            <x-base.table.td>
+                                <div class="whitespace-nowrap font-medium">
+                                    Pembelian Belum Lunas(Hutang)
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data pembelian belum dibayar
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 text-right text-danger">
+                                {{toThousand($purchaseInCompleted ?? 0)}}
+                            </x-base.table.td>
+                           
+                        </x-base.table.tr>
+                        <x-base.table.tr>
+                            <x-base.table.td>
+                                <div class="whitespace-nowrap font-medium">
+                                   Mutasi Masuk
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data transaksi lain lain mutasi masuk
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 text-right text-green-800">
+                                {{toThousand($income ?? 0)}}
+                            </x-base.table.td>
+                        </x-base.table.tr>
+                        <x-base.table.tr>
+                            <x-base.table.td>
+                                <div class="whitespace-nowrap font-medium">
+                                    Mutasi Keluar
+                                </div>
+                                <div class="mt-0.5 whitespace-nowrap text-sm text-slate-500">
+                                    data transaksi lain lain mutasi keluar
+                                </div>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-32 text-right text-danger">
+                                {{toThousand($outcome ?? 0)}}
+                            </x-base.table.td>
+                           
+                        </x-base.table.tr>
+                        <x-base.table.tr class="text-xl text-primary font-bold">
+                            <x-base.table.td>
+                                <h1>Total Saldo</h1>
+                            </x-base.table.td>
+                            <x-base.table.td class="w-52 text-right">
+                                {{toThousand($saldo ?? 0)}}
+                            </x-base.table.td>
+                           
+                        </x-base.table.tr>
+                    </x-base.table.tbody>
+                </x-base.table>
+            </div>
+
             <x-base.table class="-mt-2 border-separate border-spacing-y-[10px]">
                 <x-base.table.thead>
                     <x-base.table.tr>
@@ -125,7 +220,7 @@
                             </x-base.table.td>
                             <x-base.table.td
                                 class="w-40 border-b-0 bg-white text-center shadow-[20px_3px_20px_#0000000b] first:rounded-l-md last:rounded-r-md dark:bg-darkmode-600">
-                                {{ $item['mutation'] }}
+                                {{ toThousand($item->nominal)}} <br/>{{ $item['mutation'] }} 
                             </x-base.table.td>
                             <x-base.table.td
                                 class="relative w-56 border-b-0 bg-white py-0 shadow-[20px_3px_20px_#0000000b] before:absolute before:inset-y-0 before:left-0 before:my-auto before:block before:h-8 before:w-px before:bg-slate-200 first:rounded-l-md last:rounded-r-md dark:bg-darkmode-600 before:dark:bg-darkmode-400">
